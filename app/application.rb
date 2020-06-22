b02e695b3e10481d147afddbdfd915a4a797f9a4
eager-class-5814 <candacehchoe@gmail.com>
2020-06-22 05:15:12 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -13,7 +13,7 @@ class Application
       end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
-      resp.write handle_search(search_term)
+      resp.write handle_search(search_term)*
     elsif req.path.match(/cart/)
       if @@cart.empty?
         resp.write "Your cart is empty"
